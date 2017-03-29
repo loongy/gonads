b69e96904e01e7a923a9250d3b0912a5e9a27b99
Benjamin Wang <benjamin.jl.wang@gmail.com>
2017-03-29 21:08:05 +1100

    Silent tests

--- gonads_test.go
+++ gonads_test.go
@@ -1,37 +1,53 @@
 package gonads
 
 import "testing"
-import "log"
 
 func TestDoThenFinally(t *testing.T) {
 	wait := make(chan struct{})
 
+	do := false
+	then1 := false
+	then2 := false
+
 	Do(func() error {
-		log.Println("Do")
+		if do || then1 || then2 {
+			t.Fatal("Do must execute 1st")
+		}
+		do = true
 		return nil
 	}).Then(func() error {
-		log.Println("Then #1")
+		if !do || then1 || then2 {
+			t.Fatal("Then #1 must execute 2nd")
+		}
+		then1 = true
 		return nil
 	}).ThenAll(func() error {
-		log.Println("ThenAll #1")
+		if !do || !then1 || then2 {
+			t.Fatal("ThenAll #1 must execute 3rd, 4th, or 5th")
+		}
 		return nil
 	}, func() error {
-		log.Println("ThenAll #2")
+		if !do || !then1 || then2 {
+			t.Fatal("ThenAll #2 must execute 3rd, 4th, or 5th")
+		}
 		return nil
 	}, func() error {
-		log.Println("ThenAll #3")
-		return nil
-	}, func() error {
-		log.Println("ThenAll #4")
+		if !do || !then1 || then2 {
+			t.Fatal("ThenAll #3 must execute 3rd, 4th, or 5th")
+		}
 		return nil
 	}).Then(func() error {
-		log.Println("Then #2")
+		if !do || !then1 || then2 {
+			t.Fatal("Then #2 must execute 6th")
+		}
+		then2 = true
 		return nil
 	}).Finally(func() {
-		log.Println("Finally")
+		if !do || !then1 || !then2 {
+			t.Fatal("Finally must execute 7th")
+		}
 		wait <- struct{}{}
 	})
 
 	<-wait
-	t.Parallel()
 }
